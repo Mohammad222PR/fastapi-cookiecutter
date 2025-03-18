d6ba0adc137718d87d3654893a6dfb141916c2bc
Mohammad222PR <mohammades13851@gmail.com>
2025-03-18 21:12:51 +0330

    refactor: update docker-compose.yml and Dockerfile and fix some bugs and add bash script to run.sh and wait-for-db.sh

--- {{ cookiecutter.project_slug }}/app/core/config.py
+++ {{ cookiecutter.project_slug }}/app/core/config.py
@@ -1,16 +1,23 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+{% if cookiecutter.database == "postgres" %}
+import os
 
+pg_user = os.getenv("POSTGRES_USER", "user")
+pg_password = os.getenv("POSTGRES_PASSWORD", "password")
+pg_host = os.getenv("POSTGRES_HOST", "localhost")
+pg_database = os.getenv("POSTGRES_DB", "{{ cookiecutter.project_name }}")
+{% endif %}
 
 class Settings(BaseSettings):
     APP_NAME: str = "{{ cookiecutter.project_name }}"
     APP_VERSION: str = "{{ cookiecutter.version }}"
     ENVIRONMENT: str = "development"
-    DATABASE_URL: str = { % if cookiecutter.database == "postgres" %}"postgresql+asyncpg://user:password@localhost:5432/{{ cookiecutter.project_slug }}"
-    { % else %}"sqlite+aiosqlite:///db.sqlite3"
-    { % endif %}
-    { % if cookiecutter.use_redis == "yes" %}
+    DATABASE_URL: str = {% if cookiecutter.database == "postgres" %} f"postgresql+asyncpg://{pg_user}:{pg_password}@{pg_host}/{pg_database}"
+    {% else %} "sqlite+aiosqlite:///db.sqlite3"
+    {% endif %}
+    {% if cookiecutter.use_redis == "yes" %}
     REDIS_URL: str = "redis://localhost:6379/0"
-    { % endif %}
+    {% endif %}
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
